dfc2c41208c64c276a07eedb7befb7d963aadc11
Gaurav Tyagi <126556048+GtAtEy@users.noreply.github.com>
2025-06-23 17:04:33 +0530

    Create V20250623145530__alter_eg_pt_address_ddl.sql
    Added sql file for property service

---
+++ property-tax/property-services/src/main/resources/db/migration/main/V20250623145530__alter_eg_pt_address_ddl.sql
@@ -0,0 +1,4 @@
+ALTER TABLE public.eg_pt_address ADD column ward varchar(128) NULL;
+ALTER TABLE public.eg_pt_address ADD column colony varchar(128) NULL;
+ALTER TABLE public.eg_pt_unit ADD column rateZone varchar(128) NULL;
+ALTER TABLE public.eg_pt_unit ADD column roadFactor varchar(128) NULL;
